229cf155642fdc2255243dcc0adbcfe24f202442
Виктор3506 <vitek_m@ukr.net>
2022-11-05 17:43:59 +0200

    Start project weatherApi

---
+++ app/src/main/java/com/example/myweatherapi/view/main/MainFragment.kt
@@ -0,0 +1,41 @@
+package com.example.myweatherapi.view.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.myweatherapi.R
+import com.example.myweatherapi.viewmodel.MainViewModel
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        val observer = Observer<Any>{renderData(it)}
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+        viewModel.getWeather()
+    }
+
+    private fun renderData(data:Any){
+        Toast.makeText(requireContext(), "Work !!!", Toast.LENGTH_LONG).show()
+
+    }
+    companion object{
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+
+  }
---
+++ app/src/main/java/com/example/myweatherapi/viewmodel/MainViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.myweatherapi.viewmodel
+
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.lang.Thread.sleep
+
+class MainViewModel(private val liveData: MutableLiveData<Any> = MutableLiveData())
+    : ViewModel() {
+
+        fun getData(): LiveData<Any>{
+            return liveData
+        }
+    fun getWeather(){
+        Thread{
+            sleep(1000L)
+            liveData.postValue(Any())
+        }.start()
+    }
+}
