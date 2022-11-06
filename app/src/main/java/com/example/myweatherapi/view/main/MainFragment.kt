fda92a396cb8f61b2a39542e348bfb312b35cdfb
Виктор3506 <vitek_m@ukr.net>
2022-11-06 19:43:28 +0200

    Add AppState

--- app/src/main/java/com/example/myweatherapi/view/main/MainFragment.kt
+++ app/src/main/java/com/example/myweatherapi/view/main/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myweatherapi.view.main
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,11 +11,17 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.myweatherapi.R
 import com.example.myweatherapi.databinding.FragmentMainBinding
+import com.example.myweatherapi.viewmodel.AppState
 import com.example.myweatherapi.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
 
-    lateinit var binding: FragmentMainBinding
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
 
 
     override fun onCreateView(
@@ -30,13 +37,28 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        val observer = Observer<Any>{renderData(it)}
+        val observer = Observer<Any>{renderData(it as AppState)}
         viewModel.getData().observe(viewLifecycleOwner, observer)
         viewModel.getWeather()
     }
 
-    private fun renderData(data:Any){
-        Toast.makeText(requireContext(), "Work !!!", Toast.LENGTH_LONG).show()
+
+    private fun renderData(data:AppState){
+        when(data){
+            is AppState.Error -> {
+                binding.loadingLayout.visibility = View.GONE
+                binding.message.text = "ERROR !!!"
+
+            }
+            AppState.Loading -> {
+                binding.loadingLayout.visibility = View.VISIBLE
+            }
+            is AppState.Success -> {
+                binding.loadingLayout.visibility = View.GONE
+                binding.message.text = "SUCCESS !!!"
+            }
+        }
+
 
     }
     companion object{
